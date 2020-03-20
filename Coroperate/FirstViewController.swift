eb691ebc1b65f18aa8b7ff7b3bc3431763275d29
Lars Schwegmann <schwegmannlars@gmail.com>
2020-03-20 21:29:13 +0100

    Initial Commit

---
+++ Coroperate/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Coroperate
+//
+//  Created by Lars Schwegmann on 20.03.20.
+//  Copyright © 2020 Lars Schwegmann. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coroperate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Coroperate
+//
+//  Created by Lars Schwegmann on 20.03.20.
+//  Copyright © 2020 Lars Schwegmann. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
